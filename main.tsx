11d631d54ab045b80fa8ba27682a1bc3de35a680
smgruhn-ux <smgruhn@icloud.com>
2026-07-03 09:43:54 -0700

    Update import statement for App component

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './src/App';
+import App from './App';
 import './globals.css';
 import './typography.css';
 import './utilities.css';
