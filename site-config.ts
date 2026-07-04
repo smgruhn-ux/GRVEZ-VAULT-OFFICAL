b4fb5afb78c4af97984cf180bddeb301b26161c9
smgruhn-ux <smgruhn@icloud.com>
2026-07-04 02:44:50 +0000

    Remove old project files

--- site-config.ts
+++
@@ -1,6 +0,0 @@
-export const siteConfig = {
-  siteUrl: 'grvezvaultofficial.us',
-  contactEmail: 'contact@grvezvaultofficial.us',
-  vaultlineEmail: 'contact@vaultlineofficial.us',
-  vaultlineUrl: 'vaultlineofficial.us'
-};
