d7069aac758dbe35c706b920ea29835e2530a142
smgruhn-ux <smgruhn@icloud.com>
2026-09-06 19:53:42 -0700

    Route redacted visual detail shells

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { ContactPage } from './pages/ContactPage';
 import { ArchivePage } from './pages/ArchivePage';
 import { ManuscriptsPage } from './pages/ManuscriptsPage';
 import { MediaPage } from './pages/MediaPage';
+import { VisualDetailPage } from './pages/VisualDetailPage';
 import { PrivacyPolicyPage } from './pages/PrivacyPolicyPage';
 import { TermsOfUsePage } from './pages/TermsOfUsePage';
 import { FaqPage } from './pages/FaqPage';
@@ -21,31 +22,7 @@ import { DocumentaryQuestionPage } from './pages/DocumentaryQuestionPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
 export function App() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/about/gizzy-graves" element={<GizzyGravesPage />} />
-          <Route path="/about/dmonix" element={<DmonixPage />} />
-          <Route path="/dxxd-grvez" element={<DxxdGrvezPage />} />
-          <Route path="/music" element={<MusicPage />} />
-          <Route path="/archive" element={<ArchivePage />} />
-          <Route path="/manuscripts" element={<ManuscriptsPage />} />
-          <Route path="/relational-continuity" element={<RelationalContinuityPage />} />
-          <Route path="/films" element={<FilmsPage />} />
-          <Route path="/films/questions/:question" element={<DocumentaryQuestionPage />} />
-          <Route path="/record/:id" element={<RecordPage />} />
-          <Route path="/media" element={<MediaPage />} />
-          <Route path="/vaultline" element={<VaultlinePage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route path="/terms-of-use" element={<TermsOfUsePage />} />
-          <Route path="/faq" element={<FaqPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+  return (<BrowserRouter><Layout><Routes>
+    <Route path="/" element={<HomePage />} /><Route path="/about" element={<AboutPage />} /><Route path="/about/gizzy-graves" element={<GizzyGravesPage />} /><Route path="/about/dmonix" element={<DmonixPage />} /><Route path="/dxxd-grvez" element={<DxxdGrvezPage />} /><Route path="/music" element={<MusicPage />} /><Route path="/archive" element={<ArchivePage />} /><Route path="/manuscripts" element={<ManuscriptsPage />} /><Route path="/relational-continuity" element={<RelationalContinuityPage />} /><Route path="/films" element={<FilmsPage />} /><Route path="/films/questions/:question" element={<DocumentaryQuestionPage />} /><Route path="/record/:id" element={<RecordPage />} /><Route path="/media" element={<MediaPage />} /><Route path="/media/:slug" element={<VisualDetailPage />} /><Route path="/vaultline" element={<VaultlinePage />} /><Route path="/contact" element={<ContactPage />} /><Route path="/privacy-policy" element={<PrivacyPolicyPage />} /><Route path="/terms-of-use" element={<TermsOfUsePage />} /><Route path="/faq" element={<FaqPage />} /><Route path="*" element={<NotFoundPage />} />
+  </Routes></Layout></BrowserRouter>);
 }
